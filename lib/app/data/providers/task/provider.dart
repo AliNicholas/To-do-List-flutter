e723bb186b9337e4f4f5e84e3dd525673f1580bf
Nicholas Ali <nicholasali.888@gmail.com>
2023-06-06 00:36:59 +0700

    Todolist App flutter

---
+++ lib/app/data/providers/task/provider.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:get/get.dart';
+import 'package:todo_getx/app/core/utils/keys.dart';
+import 'package:todo_getx/app/data/services/storage.dart/services.dart';
+
+import '../../models/task.dart';
+
+class TaskProvider {
+  final _storage = Get.find<StorageService>();
+
+  // {'tasks': [
+  //   {'title': 'Work',
+  //     'color': '#ff123456',
+  //     'icon': 0xe123}
+  // ]}
+
+  List<Task> readTasks() {
+    var tasks = <Task>[];
+    jsonDecode(_storage.read(taskKey).toString())
+      .forEach((e) => tasks.add(Task.fromJson(e)));
+    return tasks;
+  }
+
+  void writeTasks(List<Task> tasks) {
+    _storage.write(taskKey, jsonEncode(tasks));
+  }
+}
